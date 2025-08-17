2a48975132120a81ab5c56c737f0a17cdeb6380e
yejelim <yj7832@korea.ac.kr>
2025-08-18 08:41:09 +0900

    setting: frontend 기본 세팅

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <div className="rounded-2xl bg-gray-100 p-6 shadow">
+        Tailwind v4 OK!
+      </div>
+    </main>
+  );
+}
